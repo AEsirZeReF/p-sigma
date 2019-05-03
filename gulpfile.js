e2c6922bf7c810e87b03f6b3a8d3718b097c7b91
Gabriel <zeref@practica>
2019-05-03 00:41:36 -0500

    Plantilla de proyecto

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync');
+const autoprefixer = require('gulp-autoprefixer');
+
+function style(){
+    return gulp.src('./index.scss')
+    .pipe(sass({
+        outputStyle:'extended',//compressed
+        sourceComments :true
+    }))
+    .pipe(rename('estilos.css'))
+    .pipe(autoprefixer({
+        versions:['last 2 browsers']
+    }))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream());
+};
+
+function watch(){
+    /*browserSync.init({
+        server:{
+            baseDir:'/'
+        }
+    });*/
+
+    gulp.watch('./index.scss',style);
+    gulp.watch('./public/index.html').on('change',browserSync.reload);
+
+};
+exports.style = style;
+exports.watch = watch;
