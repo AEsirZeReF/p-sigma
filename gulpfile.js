34e88be12df863e9a2e899e1e46a873b2c3c1641
Gabriel <zeref@practica>
2019-05-04 11:26:43 -0500

    Arreglo de auto guardado

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const browserSync = require('browser-sync');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
 
-function style(){
+/*function style(){
     return gulp.src('./index.scss')
     .pipe(sass({
         outputStyle:'extended',//compressed
@@ -17,17 +18,44 @@ function style(){
     .pipe(gulp.dest('./public/css'))
     .pipe(browserSync.stream());
 };
-
 function watch(){
-    /*browserSync.init({
+    browserSync.init({
         server:{
             baseDir:'/'
         }
-    });*/
+    });
 
     gulp.watch('./index.scss',style);
     gulp.watch('./public/index.html').on('change',browserSync.reload);
 
 };
+exports.style = style;
+exports.watch = watch;*/
+
+var paths = {
+  style: {
+    src: "./index.scss",
+    dest: "./public/css"
+  }
+};
+function style() {
+  return (
+    gulp
+      .src(paths.style.src)
+      .pipe(sass())
+      //.pipe(rename('estilos.css'))
+      //.pipe(cleanCSS())
+      .pipe(gulp.dest(paths.style.dest))
+      .pipe(browserSync.stream())
+  );
+}
+
+function watch() {
+  browserSync.init({
+    server: "./public"
+  });
+  gulp.watch(paths.style.src, style);
+  gulp.watch("./public/*.html").on("change", browserSync.reload);
+}
 exports.style = style;
 exports.watch = watch;
